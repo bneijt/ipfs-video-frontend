18c8eea3582c571b0af91779aa72f478fd3df2c4
Bram Neijt <bram@neijt.nl>
2021-06-12 10:35:13 +0200

    Add directory support

---
+++ src/IpfsLs.vue
@@ -0,0 +1,77 @@
+<template>
+  <div class="block">
+    <h1 class="title">{{ ipfsPath }}</h1>
+    <h2 class="subtitle">IPFS block listing</h2>
+    <table class="table">
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Size</th>
+          <th><abbr title="Content Identifier">CID</abbr></th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="entry in entries" v-bind:key="entry.cid">
+          <td>
+            <router-link v-bind:to="'/ipfs/' + entry.path">{{
+              entry.name
+            }}</router-link>
+          </td>
+          <td>{{ entry.size }}</td>
+          <td><code>{{ entry.cid }}</code></td>
+        </tr>
+      </tbody>
+    </table>
+    <p>{{ status }}</p>
+  </div>
+  <div class="block">
+    <path-form />
+  </div>
+</template>
+
+<script>
+function entryInformationFrom(block) {
+  var entry = {
+    path: block.path,
+    cid: block.cid.string,
+    name: block.name,
+    size: block.size,
+  };
+  return entry;
+}
+export default {
+  inject: ["ipfs"],
+  data() {
+    return {
+      status: "Initializing...",
+      entries: [],
+    };
+  },
+  mounted: function () {
+    this.getBlockInfo();
+  },
+  computed: {
+    ipfsPath: function () {
+      return this.$route.params.ipfsPath.join("/");
+    },
+  },
+  methods: {
+    async getBlockInfo() {
+      const component = this;
+      try {
+        const ipfs = await this.$ipfs,
+          ipfsPath = this.ipfsPath;
+
+        this.status = "Looking up: " + ipfsPath;
+        for await (const chunk of ipfs.ls(ipfsPath)) {
+          this.entries.push(entryInformationFrom(chunk));
+        }
+        this.status = "";
+      } catch (err) {
+        // Set error status text.
+        this.status = `Error: ${err}`;
+      }
+    },
+  },
+};
+</script>
--- src/index.js
+++ src/index.js
@@ -4,12 +4,14 @@ import App from "./App";
 import IpfsVideo from "./IpfsVideo";
 import PathForm from "./components/PathForm";
 import IpfsGatewayVideo from "./IpfsGatewayVideo";
+import IpfsLs from './IpfsLs';
 import VueIpfs from './plugins/vue-ipfs';
 
 const routes = [
   { path: '/', component: App },
   { path: '/ipfs/:ipfsPath+', component: IpfsVideo },
-  { path: '/gw/:ipfsPath+', component: IpfsGatewayVideo }
+  { path: '/gw/:ipfsPath+', component: IpfsGatewayVideo },
+  { path: '/ls/:ipfsPath+', component: IpfsLs }
 ]
 const router = createRouter({
   history: createWebHistory(),
@@ -17,7 +19,7 @@ const router = createRouter({
 })
 
 const app = createApp({});
-app.use(router)
+app.use(router);
 app.use(VueIpfs);
 app.component('path-form', PathForm);
 app.mount("#app");
