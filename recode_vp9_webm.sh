cc86d7beff7641db45889f12a7817d61da028503
Bram Neijt <bram@neijt.nl>
2021-06-15 22:31:39 +0200

    Add extra mp4 support

---
+++ recode_vp9_webm.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#Script that takes the input file and recodes to streamable VP8 Opus WEBM file
+set -e
+INPUT=$1
+if [ -z "$INPUT" ]; then
+    echo "Requires a single file as input file"
+    exit 1
+fi
+OUTPUT="${INPUT%%.*}".webm
+QUALITY=45
+ffmpeg -i "$INPUT" -c:v libvpx-vp9 -crf $QUALITY -b:v 0 -r 25 -c:a libopus -b:a 96K "$OUTPUT"
