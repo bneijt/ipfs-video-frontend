ad894a7bf4b419b5f8c268739b0a8218e4512657
Bram Neijt <bram@neijt.nl>
2022-04-06 16:21:39 +0200

    Update not found

--- src/NotFound.vue
+++ src/NotFound.vue
@@ -1,14 +1,14 @@
 <template>
   <div class="block">
-    <h1 class="title">Not found</h1>
-    <p><router-link to="/">go home</router-link></p>
+    <h1 class="title">404 Not found</h1>
+    <p>Page not found. <router-link to="/">Go back home</router-link></p>
   </div>
 </template>
 
 <script>
 export default {
   metaInfo: {
-       title: "Not found",
+       title: "404 Not found",
        description: "Page not found",
        //meta: {"http-equiv":"refresh", content:"2;url=http://example.com/"}
   },
